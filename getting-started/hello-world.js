565125a54045e8e177f50b8151b488e52ad8b5d4
Fum <66768334+funbeedev@users.noreply.github.com>
2021-10-02 15:07:32 +0100

    Update hello-world.js

--- getting-started/hello-world.js
+++ getting-started/hello-world.js
@@ -1,10 +1,14 @@
-/** 
- * Tutorial: using the log statement
- */
+/*
+-------------------------------------------------------------------------------------
+ Tutorial: using the log statement
+-------------------------------------------------------------------------------------
+*/
 
 // console.log is used to output to the console
 console.log("hello world");
 
-/** 
- * Challenge: 1. log "hello universe" to the console
- */
+/*
+-------------------------------------------------------------------------------------
+ Challenge: 1. log "hello universe" to the console
+-------------------------------------------------------------------------------------
+*/
