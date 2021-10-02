bebd69748a4aa92ad53199d8c19013e4f83f0b36
Sarah Abderemane <sarahabderemane@gmail.com>
2021-10-02 02:35:43 +0200

    Add first tutorials

--- getting-started/commenting.js
+++ getting-started/commenting.js
@@ -1,13 +1,15 @@
-/*
--------------------------------------------------------------------------------------
- Tutorial: brief description of tutorial content
--------------------------------------------------------------------------------------
-*/
+/** 
+ * Tutorial: using comments in your code
+ */
 
-/* Code here explaining concept with comments to guide */
+// Single line comments start with //. The text in the comment will be ignored by the program itself but can be used to make notes or explain what your code does.
 
-/*
--------------------------------------------------------------------------------------
- Challenge: list challenges to be completed here. minimum of one challenge per tutorial
--------------------------------------------------------------------------------------
-*/
+// I'm a single comment, yay !
+
+// Multi-line comments, start with /* and end with */. Note that it's same as the tutorial title on the top, it's a multi-line comment.
+
+
+
+/** 
+ * Challenge: 1. using single line comment to make this code say: "Hello": console.log("You")
+ */
