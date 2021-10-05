16d7687ff44d97aa83cabf75515bc8852e35c126
Saul <SaulMiquilena@users.noreply.github.com>
2021-10-05 17:56:48 -0400

    Update declaring-arrays.js
    How to create an array.

--- 3-arrays/declaring-arrays.js
+++ 3-arrays/declaring-arrays.js
@@ -4,10 +4,17 @@
 ------------------------------------------------------------------------------------
 */
 
-// Code here explaining concept with comments to guide
+// To create an array we use square brackets []
+// The objects we place inside are indexed with integer's starting at zero.
+
+let fruits = ['Apple', 'Banana']
+
+// For this example 'Apple' is indexed at position 0 and 'Banana' is indexed at position 1 inside the array fruits.
 
 /*
 ------------------------------------------------------------------------------------
  Challenge: list challenges to be completed here. minimum of one challenge per tutorial
 ------------------------------------------------------------------------------------
 */
+
+// Create an array for years that inside have 1999, 1984 and 2021, the least recent year should be at posotion 0.
