53c23eaec61c9b80929a7ed6840601d5b2b80c2f
Ivan <ivansanchez8191@gmail.com>
2021-10-07 18:59:51 +0200

    Fix lint.

--- 2-variables/creating-variables.js
+++ 2-variables/creating-variables.js
@@ -5,20 +5,20 @@
 */
 
 // To create a variable in read-only mode is used const
-const fruit = 'Apple';
+const fruit = "Apple";
 // If we try to modify the const for example fruit = 'Banana'; we will get an error that it is not possible to modify its value
 
 // If we want to initialize a variable that we can modify its value we will use let
 // The let statement declares a block-scoped local variable, optionally initializing it to a value.
 
-let fruit = 'Apple';
-fruit = 'Banana';
+let fruit = "Apple";
+fruit = "Banana";
 
 // We can also use var
 // The var statement declares a function-scoped or globally-scoped variable, optionally initializing it to a value.
 
-var fruit = 'Apple';
-fruit = 'Banana';
+var fruit = "Apple";
+fruit = "Banana";
 /*
 ------------------------------------------------------------------------------------
  Challenge: 1. Create a const variable and try to modify the value
