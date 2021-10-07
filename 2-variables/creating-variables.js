97f9ccab82db51db18027e8bb8b9c7f5207c9f2e
Ivan <ivansanchez8191@gmail.com>
2021-10-07 18:57:41 +0200

    Learn to use variables.

--- 2-variables/creating-variables.js
+++ 2-variables/creating-variables.js
@@ -1,13 +1,28 @@
 /* 
 ------------------------------------------------------------------------------------
- Tutorial: brief description of tutorial content
+ Tutorial: Create variables
 ------------------------------------------------------------------------------------
 */
 
-// Code here explaining concept with comments to guide
+// To create a variable in read-only mode is used const
+const fruit = 'Apple';
+// If we try to modify the const for example fruit = 'Banana'; we will get an error that it is not possible to modify its value
 
+// If we want to initialize a variable that we can modify its value we will use let
+// The let statement declares a block-scoped local variable, optionally initializing it to a value.
+
+let fruit = 'Apple';
+fruit = 'Banana';
+
+// We can also use var
+// The var statement declares a function-scoped or globally-scoped variable, optionally initializing it to a value.
+
+var fruit = 'Apple';
+fruit = 'Banana';
 /*
 ------------------------------------------------------------------------------------
- Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+ Challenge: 1. Create a const variable and try to modify the value
+            2. Create a let variable and modify the value
+            3. Create a var variable and modify the value
 ------------------------------------------------------------------------------------
 */
