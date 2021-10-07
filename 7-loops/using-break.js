f0f5caa492a600200f835d8ba4061c17f00948c6
SaviPrograms <serweryn123@gmail.com>
2021-10-07 07:22:15 +0200

    Add using-break tutorial

--- 7-loops/using-break.js
+++ 7-loops/using-break.js
@@ -1,13 +1,48 @@
 /* 
 ------------------------------------------------------------------------------------
- Tutorial: brief description of tutorial content
+ Tutorial: Breaking loops - break instruction
 ------------------------------------------------------------------------------------
 */
+/* Loop optimization
+This loop will find first even number. 
+Normally it would iterate through whole array, even if first number is divisible by 2.
+Break ends a loop, before doing unnecessary work. 
+*/
+const array = [0, 2, 3, 1, 7] ; //Data to search. Imagine billions of numbers 
 
-// Code here explaining concept with comments to guide
+let found;  //output variable
+for (let i = 0; i<array.length; i++ ){ //loop through whole array
+	if(array[i] % 2 == 0){ //when number in array is even
+		found = array[i];  //assign this number to output variable
+		break; //end the loop
+	} 
+	/*
+	code here wouldn't run after break
+	*/
+} 
+//first instruction after break goes here
+
+/* Escaping endless loop
+You can use break to escape endless loop
+*/
+
+let output;
+while (true) { //endless loop
+	// some code
+	
+	const data = downloadDataFromServer();
+	
+	if(data != null) {
+		output = data;
+		break; //escape the loop on data
+	} 
+	
+	// more code
+} 
+//first instruction after break
 
 /*
 ------------------------------------------------------------------------------------
- Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+ Challenge: in numbers between 0 and 200 find first, that is divisible by 2, 3, 4 and 5 at the same time 
 ------------------------------------------------------------------------------------
 */
