6f334c34bb216c7e8801a899b46555493876d131
Sourabh Kheraliya <55895224+SK02K1@users.noreply.github.com>
2021-10-08 23:55:55 +0530

    add: assignment operator tutorial

--- 5-operators/operators.js
+++ 5-operators/operators.js
@@ -4,7 +4,21 @@
 ------------------------------------------------------------------------------------
 */
 
-// Code here explaining concept with comments to guide
+/* 
+------------------------------------------------------------------------------------
+ 🌟ASSIGNMENT OPERATOR "=":-
+
+ - The assignment operator is used to assign value that is on the right of the operator to the left.
+
+ 🚨THINGS TO NOTICE:- 
+
+ - If there are any calculations/expression to the right of the assignment operator, those are performed/resolved before assigning it to the left of the operator.
+------------------------------------------------------------------------------------
+*/
+
+// ASSIGNMENT OPERATOR EXAMPLES
+let num1 = 6; // assigning 6 to the num1 variable
+let num2 = 5; // assigning 5 to the num2 variable
 
 /*
 ------------------------------------------------------------------------------------
