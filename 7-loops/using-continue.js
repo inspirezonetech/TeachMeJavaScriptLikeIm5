4635ad87e41bd8157fc5e53a7aa435d4f43af087
SaviPrograms <42438445+SaviPrograms@users.noreply.github.com>
2021-10-10 01:24:25 +0200

    Add continue instruction tutorial

--- 7-loops/using-continue.js
+++ 7-loops/using-continue.js
@@ -1,13 +1,38 @@
 /* 
 ------------------------------------------------------------------------------------
- Tutorial: brief description of tutorial content
+ Tutorial: Skipping iterations - continue instruction
 ------------------------------------------------------------------------------------
 */
 
-// Code here explaining concept with comments to guide
+/*
+Loop optimalization
+*/
 
+// Imagine very long array
+let arr = [
+    'hello',
+    42,
+    108,
+    'number',
+    "world"
+]
+
+let output = "";
+
+for(let i = 0; i < arr.length; i++){ //Iterates trought whole array
+    if(typeof arr[i] != String) //if type of data is wrong for doesn't have to do anything more
+        continue; //so it skips to next iteration
+    
+    /* 
+    ...code... 
+    */
+
+    output += arr[i].toLowerCase() + " ";
+}
+
+console.log(output);
 /*
 ------------------------------------------------------------------------------------
- Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+ Challenge: create script that adds every cube of squere roots of numbers form array [2, 'car', 3, "cat", 81, true, 9]
 ------------------------------------------------------------------------------------
 */
