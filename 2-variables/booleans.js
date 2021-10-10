3d3bedd168edb4fb88847582933af1e43cb4d25b
Raghav-Modani <raghavmodani4@gmail.com>
2021-10-10 21:24:29 +0530

    added boolean tutorial with challenges

--- 2-variables/booleans.js
+++ 2-variables/booleans.js
@@ -1,13 +1,41 @@
 /* 
 ------------------------------------------------------------------------------------
- Tutorial: brief description of tutorial content
+ Tutorial: Booleans in JavaScript
 ------------------------------------------------------------------------------------
 */
+// Definition:-
+// The boolean (not Boolean) is a primitive data type in JavaScript. It can have only two values: "true" or "false". 
+// It is useful in controlling program flow using conditional statements.
 
-// Code here explaining concept with comments to guide
+
+// DECLARATION:-
+var YES = true;
+
+var NO = false;
+
+// CHECK TYPE OF VARIABLE WITH "typeof" OPERATOR:-
+console.log(typeof(YES)); //will print "boolean" in the console
+console.log(typeof(NO)); //will print "boolean" in the console
+
+// COMPARISION OPERATORS AND BOOLEANS:-
+//The comparison expressions return boolean values to indicate whether the comparison is "true" or "false". 
+//For example, the following expressions return boolean values.
+var result = 1 > 2; //result stores "false"
+
+// BOOLEAN FUNCTION:-
+//JavaScript provides the Boolean() function that converts other types to a boolean type.
+//The value specified as parameter to the Boolean() function  will be converted to a boolean value.
+//The Boolean() will return "true" for any non-empty, non-zero, object, or array and will return "false" otherwise.
+
+var x = Boolean("Hello"); // x stores "true"
+var y = Boolean(10); // y stores "true"
+
 
 /*
 ------------------------------------------------------------------------------------
- Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+ Challenge:-
+ // 1. Check the boolean value of "0" and "-0" using the Boolean() function.
+ // 2. Check the boolean value of "" (empty string) using the Boolean() function. 
+ // 3. Check the boolean value of "[]" (empty array) using the Boolean() function. 
 ------------------------------------------------------------------------------------
 */
