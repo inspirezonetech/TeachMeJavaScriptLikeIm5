f23e9efb67205c6ac5c2033c692b98d85be796bd
Iván Sánchez Salvador <ivan.sanchez@mimacom.com>
2021-10-11 15:28:58 +0200

    Fix variables.

--- 2-variables/creating-variables.js
+++ 2-variables/creating-variables.js
@@ -5,20 +5,20 @@
 */
 
 // To create a variable in read-only mode is used const
-const fruit = "Apple";
+const fruitConst = "Apple";
 // If we try to modify the const for example fruit = 'Banana'; we will get an error that it is not possible to modify its value
 
 // If we want to initialize a variable that we can modify its value we will use let
 // The let statement declares a block-scoped local variable, optionally initializing it to a value.
 
-let fruit = "Apple";
-fruit = "Banana";
+let fruitLet = "Apple";
+fruitLet = "Banana";
 
 // We can also use var
 // The var statement declares a function-scoped or globally-scoped variable, optionally initializing it to a value.
 
-var fruit = "Apple";
-fruit = "Banana";
+var fruitApple = "Apple";
+fruitApple = "Banana";
 /*
 ------------------------------------------------------------------------------------
  Challenge: 1. Create a const variable and try to modify the value
