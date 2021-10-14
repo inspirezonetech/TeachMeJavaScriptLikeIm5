355e271ea69db62306d538e89a3b586feab8b58c
Dilrose Reji <dilrose2000@gmail.com>
2021-10-14 14:47:06 +0530

    added while-loops.js

--- 7-loops/while-loops.js
+++ 7-loops/while-loops.js
@@ -1,13 +1,25 @@
 /* 
 ------------------------------------------------------------------------------------
- Tutorial: brief description of tutorial content
+ Tutorial: To understand the working of while-loops
 ------------------------------------------------------------------------------------
 */
+// Loops are used to run the same code again and again, each time with different or same values.
+// The while loop loops through the block till it satisfies the condition
 
-// Code here explaining concept with comments to guide
+// print even numbers until 20
+var i = 0;  // initialized a variable with zero
+
+// while loop runs from i = 0 to 18 because we want numbers until 20
+// if we run it till i=20, we will get 22 as well in the output.
+
+while(i<20){
+    console.log(i);
+    i = i+2;        // that means this loop will automatically terminate when i becomes 20 since condition is not satisfied
+}
 
 /*
 ------------------------------------------------------------------------------------
- Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+ Challenge 1: Display all odd numbers from 1 to 100
+ Challenge 2: Find the minimum number in an array by iterating through elements
 ------------------------------------------------------------------------------------
 */
