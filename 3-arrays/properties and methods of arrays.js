4f8916e80632abc47f6443d74cc9844fb24f873b
Rishabh Jaiswal <92149073+rishabh1911007@users.noreply.github.com>
2022-10-14 20:32:53 +0530

    Adding a file which states the properties of arrays

---
+++ 3-arrays/properties and methods of arrays.js
@@ -0,0 +1,54 @@
+// Some properties and methods of array are--
+
+
+// 1. Returns the number of elements-
+
+length
+
+//example--
+
+fruits.length;
+
+
+
+
+//2. Sorts the array-
+
+sort()
+
+//example--
+
+fruits.sort();
+
+
+
+
+// 3. Joins arrays and returns an array with the joined arrays-
+
+concat()
+
+//example--
+
+fruits.concat();
+
+
+
+
+// 4. Joins all elements of an array into a string-
+
+join()
+
+//example--
+
+fruits.join();
+
+
+
+
+// 5. Search the array for an element and returns its position-
+
+indexOf()
+
+//example--
+
+fruits.indexOf();
