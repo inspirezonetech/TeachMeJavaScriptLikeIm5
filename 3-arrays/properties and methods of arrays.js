ea3304e37d90ab8dee72977c0d8efbdc3838b069
Rishabh Jaiswal <92149073+rishabh1911007@users.noreply.github.com>
2022-10-16 00:06:28 +0530

    Added a file which states the properties and method of arrays

---
+++ 3-arrays/properties and methods of arrays.js
@@ -0,0 +1,64 @@
+// Some properties and methods of array are--
+
+
+// 1. Returns the number of elements-
+
+		//length
+
+//example--
+
+var fruits = ["Orange", "Apple", "Banana"];
+
+fruits.length=2;
+
+
+
+
+//2. Sorts the array-
+
+		//sort();
+
+//example--
+
+var fruits = ["Orange", "Apple", "Banana"];
+
+fruits.sort();
+
+
+
+
+// 3. Joins arrays and returns an array with the joined arrays-
+
+		//concat();
+
+//example--
+
+var fruits = ["Orange", "Apple", "Banana"];
+
+fruits.concat();
+
+
+
+
+// 4. Joins all elements of an array into a string-
+
+		//join();
+
+//example--
+
+var fruits = ["Orange", "Apple", "Banana"];
+
+fruits.join();
+
+
+
+
+// 5. Search the array for an element and returns its position-
+
+		//indexOf();
+
+//example--
+
+var fruits = ["Orange", "Apple", "Banana"];
+
+fruits.indexOf();
